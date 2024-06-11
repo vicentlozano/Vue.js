81d1a734df2bbaadd119922bcef47e2414ac801f
vicentlozano <lozanodroid@gmail.com>
2024-06-11 17:52:19 +0200

    Final del repaso de JavaScript-TypeScript

--- 01-bases/src/bases/10-axios.ts
+++ 01-bases/src/bases/10-axios.ts
@@ -3,13 +3,11 @@ import type { GIFResponse } from "../interfaces/gif.response";
 
 const apiKey = "OyM07mKNg8221O5nudteG6CdSEJ7VrcY";
 export const giphyAPi = axios.create({
-    baseURL: 'https://api.giphy.com/v1/gifs',
-    params: {
-        apiKey: apiKey,
-    }
-}
-
-);
+  baseURL: "https://api.giphy.com/v1/gifs",
+  params: {
+    apiKey: apiKey,
+  },
+});
 // giphyAPi.get<GIFResponse>('/random')
 //  .then(resp => console.log(resp.data.data.images.downsized_large.url))
 //  .catch(err => console.log(err));
